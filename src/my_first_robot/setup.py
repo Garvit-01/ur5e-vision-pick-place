1866dc334b6f74972dd5158c9383834f4e78ffad
Garvit Katyal <garvitkatyal2@gmail.com>
2026-07-01 21:08:24 +0000

    Adding URDF file and RViz visualization Done

--- src/my_first_robot/setup.py
+++ src/my_first_robot/setup.py
@@ -11,6 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/robot.launch.py']),
+        ('share/' + package_name + '/urdf', ['urdf/my_robot.urdf']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
