ef052180c9e47da395986789b7d27c8f24aabb79
Garvit Katyal <garvitkatyal2@gmail.com>
2026-07-12 20:23:31 +0000

    Add Cartesian space motion planning - real IK working

--- src/my_first_robot/setup.py
+++ src/my_first_robot/setup.py
@@ -34,7 +34,8 @@ setup(
         'gripper_client = my_first_robot.gripper_service_client:main',
         'move_action_server = my_first_robot.move_action_server:main',
         'move_action_client = my_first_robot.move_action_client:main',
-        'moveit_commander = my_first_robot.moveit_commander:main'
+        'moveit_commander = my_first_robot.moveit_commander:main',
+        'moveit_cartesian = my_first_robot.moveit_cartesian:main'
     ],
 },
 )
