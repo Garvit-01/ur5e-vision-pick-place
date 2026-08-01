68062743bfeac4c555648763e038a839bdc07f19
Garvit Katyal <garvitkatyal2@gmail.com>
2026-08-01 21:25:05 +0000

    Add UR5e + Robotiq combined MoveIt config package
    Built via MoveIt Setup Assistant: two planning groups (ur_manipulator,
    robotiq_gripper), gripper mimic joints correctly marked passive,
    end effector/controllers configured. Verified working via view_frames
    and live RViz motion testing. Also fixes ur5e_robotiq.urdf.xacro never
    being installed (missing from setup.py's data_files).

--- src/my_first_robot/setup.py
+++ src/my_first_robot/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/robot.launch.py']),
-        ('share/' + package_name + '/urdf', ['urdf/my_robot.urdf']),
+        ('share/' + package_name + '/urdf', ['urdf/my_robot.urdf','urdf/ur5e_robotiq.urdf.xacro']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
